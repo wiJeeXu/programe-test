3d38cb6fc2aed941605bc995f7716b1af16dd4ee
xuweijie <18751297674@163.com>
2023-08-31 09:54:24 +0800

    abc

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -1,52 +1,52 @@
 <template>
-  <div class="app">
-    <div class="sidebar">
-      <button @click="addStackContainer">Add Stack Container</button>
-      <button @click="addFreeContainer">Add Free Container</button>
-    </div>
-
-    <div class="canvas">
-      <draggable v-model="components" group="canvas">
-        <component
-          v-for="item in components"
-          :key="item.id"
-          :is="item.type"
-          :data="item.data"
-          @add="addInsideComponent(item.id)"
-        />
-      </draggable>
-    </div>
+  <div>
+    <draggable
+      class="list-group"
+      :list="list1"
+      :group="{ name: 'people', pull: 'clone', put: false }"
+      @change="log"
+      itemKey="name"
+    >
+      <template #item="{ element, index }">
+        <div class="list-group-item">{{ element.name }} {{ index }}</div>
+      </template>
+    </draggable>
+    <draggable class="list-group" :list="list2" group="people" @change="log" itemKey="name">
+      <template #item="{ element, index }">
+        <div class="list-group-item">{{ element.name }} {{ index }}</div>
+      </template>
+    </draggable>
   </div>
 </template>
-
 <script lang="ts" setup>
 import { ref } from 'vue'
-import Draggable from 'vuedraggable'
-
-const components: any = ref([])
-
-const addStackContainer = () => {
-  components.value.push({
-    id: Date.now(),
-    type: 'StackContainer',
-    data: []
-  })
-}
-
-const addFreeContainer = () => {
-  components.value.push({
-    id: Date.now(),
-    type: 'FreeContainer',
-    data: []
-  })
-}
-
-const addInsideComponent = (containerId: number) => {
-  const container = components.value.find((c: any) => c.id === containerId)
-  if (container) {
-    container.data.push({
-      /* your component data */
-    })
-  }
+import draggable from 'vuedraggable'
+const list1 = ref([
+  { name: 'John', id: 1 },
+  { name: 'Joao', id: 2 },
+  { name: 'Jean', id: 3 },
+  { name: 'Gerard', id: 4 }
+])
+const list2 = ref([
+  { name: 'Juan', id: 5 },
+  { name: 'Edgard', id: 6 },
+  { name: 'Johnson', id: 7 }
+])
+function log(evt) {
+  window.console.log(evt)
 }
 </script>
+<style scoped>
+.buttons {
+  margin-top: 35px;
+}
+
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+
+.not-draggable {
+  cursor: no-drop;
+}
+</style>
